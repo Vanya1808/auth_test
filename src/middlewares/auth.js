0aed17fc584dd7ca68a551870944fb4ea9ef6b37
Vanya <56477758+Vanya1808@users.noreply.github.com>
2022-02-17 21:10:31 +0200

    Add files via upload

---
+++ src/middlewares/auth.js
@@ -0,0 +1,18 @@
+// Реалізуйте мідлвер для перевірки авторизації користувача
+// У випадку неавторизованого користувача вертаємо помилку 401
+// В іншому випадку продовжуємо обробку запиту
+const passport = require('passport');
+
+module.exports = (req, res, next) => {
+    passport.authenticate('jwt', { session: false }, (error, user) => {
+      if (error || !user) {
+        res.status(401).json({
+          message: 'Unauthorized',
+        });
+        return;
+      }
+  
+      req.user = user;
+      next();
+    })(req, res, next);
+};
